3a82e7f505c44a7cc3bb688453e79cdd0820b068
keiichiro shikai <nb.jtktn.1211@gmail.com>
2026-07-17 00:13:09 +0900

    feat:マイグレーションの作成 #2

--- database/migrations/2026_07_15_093842_create_reviews_table.php
+++ database/migrations/2026_07_15_093842_create_reviews_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken()->nullable();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('book_id')->constrained()->cascadeOnDelete();
+            $table->tinyInteger('rating')->unsigned();
+            $table->text('comment')->nullable();
             $table->timestamps();
+
+            $table->unique(['user_id', 'book_id']);
         });
     }
 
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reviews');
     }
 };
