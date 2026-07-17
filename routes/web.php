2cca4965e3c7a7c68675cfd624d9d52e0af8e866
keiichiro shikai <nb.jtktn.1211@gmail.com>
2026-07-18 02:48:16 +0900

    feat: Laravel Fortifyを導入し認証機能を実装

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/books', function () {
+    return view('books.index');
+})->name('books.index');
