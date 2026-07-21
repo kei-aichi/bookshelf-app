418de68b7b208b2f09261d032ed52bb1127e7da0
keiichiro shikai <nb.jtktn.1211@gmail.com>
2026-07-22 01:17:06 +0900

    書籍一覧画面の表示機能を実装

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,39 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Models\Book;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// 公開ページ
+Route::get('/', [BookController::class, 'index'])
+    ->name('home');
 
-Route::get('/books', function () {
-    return view('books.index');
-})->name('books.index');
+Route::get('/books', [BookController::class, 'index'])
+    ->name('books.index');
+
+Route::get('/ranking', function () {
+    return view('ranking.index');
+})->name('ranking.index');
+
+// ログイン必須ページ
+Route::middleware('auth')->group(function () {
+    Route::get('/books/create', function () {
+        return view('books.create');
+    })->name('books.create');
+
+    Route::get('/books/{book}/edit', function () {
+        return view('books.edit');
+    })->name('books.edit');
+
+    Route::get('/favorites', function () {
+        return 'お気に入り一覧は今後実装予定です';
+    })->name('favorites.index');
+
+    Route::get('/genres', function () {
+        return view('genres.index');
+    })->name('genres.index');
+});
+
+Route::get('/books/{book}', function (Book $book) {
+    return '書籍詳細画面：'.$book->title;
+})->name('books.show');
